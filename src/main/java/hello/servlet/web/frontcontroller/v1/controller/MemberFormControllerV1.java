5f4b7d5fa0e17d48e933882980ac83229936a8ce
Kimdonghun <zhfptm12@naver.com>
2022-11-18 00:41:02 +0900

    1118

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -1,19 +1,19 @@
 package hello.servlet.web.frontcontroller.v1.controller;
 
 import hello.servlet.web.frontcontroller.v1.ControllerV1;
-import lombok.SneakyThrows;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.rmi.ServerException;
 
-public class MemberFromControllerV1 implements ControllerV1 {
+public class MemberFormControllerV1 implements ControllerV1 {
 
     @Override
-    public void process(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException {
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
     }
 }
